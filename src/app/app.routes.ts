702d4481b48fdefc9c1e7f2cb4f66fbf131f3466
Denis Kashchavtsev <dkashavtsev@gmail.com>
2025-08-05 23:17:12 +0300

    refactoring styles and adding about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PostComponent } from './pages/post/post.component';
 import { PostsListComponent } from './pages/posts-list/posts-list.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'posts', component: PostsListComponent },
   { path: 'blog/:slug', component: PostComponent },
+  { path: 'about', component: AboutComponent },
 ];
